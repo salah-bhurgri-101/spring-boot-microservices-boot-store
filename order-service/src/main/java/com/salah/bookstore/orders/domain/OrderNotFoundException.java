8cfb549d5692f454abcbb87d7024bcfce2ba446c
Salah <bhurgrisalah@gmail.oom>
2025-06-09 23:28:40 +0500

    new one

---
+++ order-service/src/main/java/com/salah/bookstore/orders/domain/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package com.salah.bookstore.orders.domain;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderNumber(String orderNumber) {
+        return new OrderNotFoundException("Order with Number " + orderNumber + " not found");
+    }
+}
